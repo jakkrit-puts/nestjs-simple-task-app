551b1625a252c3a24dcb6dcd2e61c1a1f25ac597
jakkrit putsorn <jakkrit.puts@gmail.com>
2022-08-24 17:58:55 +0700

    task add guard Jwt

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,12 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './entity/task.entity';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt-auth.guard';
 
 @Controller('task')
 export class TaskController {
   constructor(private taskService: TaskService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   createTask(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
     return this.taskService.createTask(createTaskDto);
